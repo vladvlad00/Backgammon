3ad815e170a439c27c5afe1592d17bdcf04a9bd0
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-22 16:11:27 +0300

    add integration functions for register / login

--- src/main/java/front/utils/NetworkManager.java
+++ src/main/java/front/utils/NetworkManager.java
@@ -1,17 +1,38 @@
 package front.utils;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class NetworkManager {
-    private static final String URL = "localhost:8081/";
+    private static final String URL = "http://localhost:8081";
 
     public static String login(String username, String password) {
-        //If 200 -> return "ok";
-        //else -> return "err";
-        return "ceva";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //TODO de facut mai elegant aici cu o clasa de user
+        HttpEntity<String> request = new HttpEntity<>("{ \"username\": \"" + username +
+                "\", \"password\": \"" + password + "\" }", headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(URL + "/login", request, String.class);
+        if (response.getStatusCode().is2xxSuccessful())
+            return "ok " + response.getBody();
+        return "err";
     }
 
     public static String register(String username, String password) {
-        //If 200 -> return "ok";
-        //else -> return "err";
-        return "ceva";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //TODO de facut mai elegant aici cu o clasa de user
+        HttpEntity<String> request = new HttpEntity<>("{ \"username\": \"" + username +
+                "\", \"password\": \"" + password + "\" }", headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(URL + "/register", request, String.class);
+        if (response.getStatusCode().is2xxSuccessful())
+            return "ok";
+        return "err";
     }
 }
