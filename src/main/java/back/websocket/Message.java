d43d896539c2dba619b3c765af64d2296afc57ae
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-27 11:56:32 +0300

    start websocket support implementation

---
+++ src/main/java/back/websocket/Message.java
@@ -0,0 +1,25 @@
+package back.websocket;
+
+public class Message
+{
+    private String content;
+
+    public Message()
+    {
+    }
+
+    public Message(String content)
+    {
+        this.content = content;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+}
