07f7a6542a557974c3e106a0835652c7e6e710bc
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-30 12:54:44 +0300

    finish lobby integration

--- src/main/java/front/utils/NetworkManager.java
+++ src/main/java/front/utils/NetworkManager.java
@@ -10,8 +10,9 @@ import front.entities.UserRole;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.persistence.Lob;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NetworkManager {
     private static final String URL = "http://localhost:8081";
@@ -45,10 +46,11 @@ public class NetworkManager {
 
     public static List<Lobby> getAllLobbies() {
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(User.getInstance().getToken());
+        HttpHeaders headers = getHeaders();
         HttpEntity entity = new HttpEntity(headers);
         ResponseEntity<String> response = restTemplate.exchange(URL + "/room", HttpMethod.GET, entity, String.class);
+        if (!response.getStatusCode().is2xxSuccessful())
+            return null;
         ObjectMapper objectMapper = new ObjectMapper();
         try
         {
@@ -61,13 +63,35 @@ public class NetworkManager {
         }
     }
 
-    public static String joinThroughID(Long roomID, UserRole role) {
-        //TODO: join room
-        return null;
+    public static String joinThroughID(Long roomId, UserRole role) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = getHeaders();
+        Map<String, String> requestJson = new HashMap<>();
+        requestJson.put("roomId", roomId.toString());
+        requestJson.put("role", role.name());
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestJson, headers);
+        ResponseEntity<String> response = restTemplate.exchange(URL + "/user/room", HttpMethod.PUT, request, String.class);
+        if (!response.getStatusCode().is2xxSuccessful())
+            return null;
+        return response.getBody();
     }
 
     public static Lobby createRoom(String roomName) {
-        //TODO: create room
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = getHeaders();
+        Map<String, String> requestJson = new HashMap<>();
+        requestJson.put("name", roomName);
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestJson, headers);
+        ResponseEntity<Lobby> response = restTemplate.postForEntity(URL + "/room", request, Lobby.class);
+        if (!response.getStatusCode().is2xxSuccessful())
+            return null;
+        return response.getBody();
+    }
+
+    private static HttpHeaders getHeaders()
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(User.getInstance().getToken());
+        return headers;
     }
 }
