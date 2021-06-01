12879e18be6097a3bd2a568d31edf742d0417948
Andreizabo <45977666+Andreizabo@users.noreply.github.com>
2021-06-01 21:02:20 +0300

    [FIX] player leaving game

--- src/main/java/front/GraphicsMain.java
+++ src/main/java/front/GraphicsMain.java
@@ -1,14 +1,18 @@
 package front;
 
+import front.entities.LobbyUser;
 import front.entities.User;
+import front.entities.UserRole;
 import front.utils.Cookies;
-import front.utils.handlers.LobbyHandler;
-import front.utils.handlers.NetworkManager;
-import front.utils.handlers.SceneHandler;
+import front.utils.handlers.*;
+import front.utils.websocket.Message;
+import front.utils.websocket.WSClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GraphicsMain extends Application {
     public static void main(String[] args) {
@@ -30,6 +34,33 @@ public class GraphicsMain extends Application {
     public void stop() {
         if(User.getInstance().getInRoom() != null && User.getInstance().getInRoom()) {
             NetworkManager.leaveRoom();
+        }
+        if(FrameHandler.getMainGameFrame().getLobby() != null) {
+            for(LobbyUser lobbyUser : FrameHandler.getMainGameFrame().getLobby().getUsers()) {
+                if(lobbyUser.getUsername().equals(User.getInstance().getUsername())) {
+                    if(FrameHandler.getMainGameFrame().getLobby().getRoleOfUser(User.getInstance().getUsername()).equals(UserRole.SPECTATOR) || FrameHandler.getMainGameFrame().getLobby().getRoleOfUser(User.getInstance().getUsername()).equals(UserRole.HOST_SPECTATOR)) {
+                        Map<String, String> options = new HashMap<>();
+                        options.put("count", "-1");
+                        WSClient.getInstance().sendMessage(new Message("spectator", options));
+                        FrameHandler.getMainMenuFrame().goToMenu();
+                        NetworkManager.leaveRoom();
+                        SceneHandler.changeScene("menu");
+                    }
+                    else {
+                        Map<String, String> options = new HashMap<>();
+                        String color;
+                        if(GameHandler.getBlackUser().getUsername().equals(User.getInstance().getUsername())) {
+                            color = "black";
+                        }
+                        else {
+                            color = "white";
+                        }
+                        options.put("color", color);
+                        options.put("user", User.getInstance().getUsername());
+                        WSClient.getInstance().sendMessage(new Message("disconnect", options));
+                    }
+                }
+            }
         }
     }
 }
